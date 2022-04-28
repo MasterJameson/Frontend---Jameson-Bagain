5ab526b605cc5b550e422355d34f48ba8e993d03
Master <bagainjameson@gmail.com>
2022-04-28 12:50:01 +0800

    added active user in landing page

--- src/View/LandingPage/LandingPage.jsx
+++ src/View/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
 import getApp from '../getElement';
 import { makeStyles } from '@mui/styles';
@@ -17,7 +17,7 @@ const useStyles = makeStyles((theme) => ({
   listStyle: {
     display: 'flex',
     justifyContent: 'center',
-    marginBottom: 229,
+    marginBottom: 190,
   },
   content: {
     paddingLeft: 6
@@ -31,6 +31,8 @@ const LandingPage = () => {
   const navigate = useNavigate();
   const classes = useStyles()
   const token = localStorage.getItem('token')
+  const [responseApi, setResponseApi] = useState()
+
 
   useEffect(() => {
     setTimeout(() => {
@@ -43,11 +45,24 @@ const LandingPage = () => {
       }
     }, 1000)
   })
+  useEffect(() => {
+    if (token) {
+      const bearer = 'Bearer ' + token;
+      const url = 'https://astibot.unidelc.com/getusers.php'
+      fetch(url, {
+        method: 'GET',
+        headers: { 'Authorization': bearer, 'Content-Type': 'application/json' },
+      }).then(response => response.json()).then(data => setResponseApi(data))
+
+    }
+  }, [token])
   console.log('token', token)
+  console.log('token', responseApi)
 
   return (
     <>
       <h1 className={classes.titleH1}>This is our great company.</h1>
+      <p style={{margin: '20px', color: '#fff'}}>{responseApi !== undefined && responseApi.user.name}</p>
       <ul className={classes.listStyle}>
         <li className={classes.content}><img src={fcIcon} alt="" /></li>
         <li className={classes.content}><img src={twitterIcon} alt="" /></li>
